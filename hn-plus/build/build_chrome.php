011ec90665a3ea14c2de6f65a64bf16a14a9e7c9
hn-plus <hn-plus@darklaunch.com>
2014-01-15 23:03:38 -0800

    Clean up

--- hn-plus/build/build_chrome.php
+++ hn-plus/build/build_chrome.php
@@ -232,5 +232,5 @@ foreach ($javascript_files as $source => $destination) {
 
 // Copy files
 copy('../src/chrome/background.html', '../built/chrome/hn-comment-folding/1.0.0_0/background.html');
-copy('../src/chrome/popup.html', '../built/chrome/hn-comment-folding/1.0.0_0/popup.html');
-copy('../src/chrome/manifest.json', '../built/chrome/hn-comment-folding/1.0.0_0/manifest.json');
+copy('../src/chrome/popup.html',      '../built/chrome/hn-comment-folding/1.0.0_0/popup.html');
+copy('../src/chrome/manifest.json',   '../built/chrome/hn-comment-folding/1.0.0_0/manifest.json');
