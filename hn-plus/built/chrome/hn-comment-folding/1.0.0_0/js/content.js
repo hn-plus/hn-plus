1c7358dd6fb4939abf3b5cfad6e90903f3016cda
hn-plus <hn-plus@darklaunch.com>
2015-09-17 23:49:10 -0700

    Reuse vim navigation for both home page and item page

--- hn-plus/built/chrome/hn-comment-folding/1.0.0_0/js/content.js
+++ hn-plus/built/chrome/hn-comment-folding/1.0.0_0/js/content.js
@@ -1,6 +1,51 @@
 /*! Example v1.0.0 | (c) 2015 Example, Inc. | example.com/license */
+var keyCode = {
+    BACKSPACE: 8,
+    COMMA: 188,
+    DELETE: 46,
+    DOWN: 40,
+    END: 35,
+    ENTER: 13,
+    ESCAPE: 27,
+    FORWARD_SLASH: 191,
+    HOME: 36,
+    LEFT: 37,
+    PAGE_DOWN: 34,
+    PAGE_UP: 33,
+    PERIOD: 190,
+    RIGHT: 39,
+    SPACE: 32,
+    TAB: 9,
+    UP: 38,
+    A: 65,
+    B: 66,
+    C: 67,
+    D: 68,
+    E: 69,
+    F: 70,
+    G: 71,
+    H: 72,
+    I: 73,
+    J: 74,
+    K: 75,
+    L: 76,
+    M: 77,
+    N: 78,
+    O: 79,
+    P: 80,
+    Q: 81,
+    R: 82,
+    S: 83,
+    T: 84,
+    U: 85,
+    V: 86,
+    W: 87,
+    X: 88,
+    Y: 89,
+    Z: 90,
+};
 function openBackgroundTab( url ) {
-    console.info( 'openBackgroundTab', url );
+    console.info( 'openBackgroundTab:', url );
     chrome.runtime.sendMessage({
         'action': 'chrome.tabs.create',
         'createProperties': {
@@ -9,112 +54,63 @@ function openBackgroundTab( url ) {
         },
     });
 }
-var loc = window.location.toString();
-if ( /^https:\/\/news\.ycombinator\.com\/$/.test( loc ) ) {
-    console.info( 'home page' );
-    var keyCode = {
-        BACKSPACE: 8,
-        COMMA: 188,
-        DELETE: 46,
-        DOWN: 40,
-        END: 35,
-        ENTER: 13,
-        ESCAPE: 27,
-        FORWARD_SLASH: 191,
-        HOME: 36,
-        LEFT: 37,
-        PAGE_DOWN: 34,
-        PAGE_UP: 33,
-        PERIOD: 190,
-        RIGHT: 39,
-        SPACE: 32,
-        TAB: 9,
-        UP: 38,
-        A: 65,
-        B: 66,
-        C: 67,
-        D: 68,
-        E: 69,
-        F: 70,
-        G: 71,
-        H: 72,
-        I: 73,
-        J: 74,
-        K: 75,
-        L: 76,
-        M: 77,
-        N: 78,
-        O: 79,
-        P: 80,
-        Q: 81,
-        R: 82,
-        S: 83,
-        T: 84,
-        U: 85,
-        V: 86,
-        W: 87,
-        X: 88,
-        Y: 89,
-        Z: 90,
-    };
-    var homePageLinks = $( '.title > a' );
-    var commentLinks = $( '.subtext a[href^=item\\?id\\=]' );
-    var links = $().add(homePageLinks).add(commentLinks);
-    $( links ).each(function() {
-        $(this).click(function( event ) {
-            console.log( event.target );
-            event.preventDefault();
-            console.log( 'current position:', currentPosition );
-            if ( event.target.parentElement.classList.contains( 'title' ) ) {
-                currentPosition = entries.index( event.target.parentElement.parentElement );
-            } else {
-                currentPosition = entries.index( $( event.target ).parents( 'tr' ).prev() );
+var vimNavigation = {
+    currentPosition: -1,
+    init: function( entries ) {
+        console.info( 'init' );
+        this._addStyle();
+        this._setEntries( entries );
+        this._bindKeyboardEvents();
+    },
+    _addStyle: function() {
+        var style = document.createElement( 'style' );
+        style.type = 'text/css';
+        style.innerHTML =
+            '._vimNavActive {' +
+                'background-color: #bbccff;' +
+            '}' +
+            '.athing > td {' +
+                'border-bottom: 1px solid transparent;' +
+                'border-top: 1px solid transparent;' +
+            '}' +
+            '._vimNavActive > td {' +
+                'border-bottom: 1px solid #888888;' +
+                'border-top: 1px solid #888888;' +
+            '}' +
+            '._vimNavActive > td:first-child {' +
+                'border-left: 1px solid #888888;' +
+            '}' +
+            '._vimNavActive > td:last-child {' +
+                'border-right: 1px solid #888888;' +
+            '}' +
+            '';
+        var head = document.getElementsByTagName( 'head' )[ '0' ];
+        head.appendChild( style );
+    },
+    _bindKeyboardEvents: function() {
+        console.info( '_bindKeyboardEvents' );
+        $( document ).keyup(function( event ) {
+            if ( event.which === keyCode.J ) {
+                vimNavigation._goToNext();
+            } else if ( event.which === keyCode.K ) {
+                vimNavigation._goToPrevious();
+            } else if ( event.which === keyCode.G && event.shiftKey ) {
+                vimNavigation._goToLast();
             }
-            moveToPosition( currentPosition );
-            openBackgroundTab( $(this).prop( 'href' ) );
         });
-    });
-    var $searchInput = $( '<input placeholder="Search" type="text" />' );
-    $searchInput.keypress(function( event ) {
-        if ( event.which === keyCode.ENTER ) {
-            console.log( 'enter pressed', $(this).val() );
-            window.location = 'https://hn.algolia.com/#!/all/forever/0/' + encodeURIComponent( $(this).val() );
-        }
-    });
-    $( '.pagetop:first' ).append( ' | ' ).append( $searchInput ).parent().css({'white-space': 'nowrap'});
-    var style = document.createElement( 'style' );
-    style.type = 'text/css';
-    style.innerHTML =
-        '.athing.active {' +
-            'background-color: #bbccff;' +
-        '}' +
-        '.athing > td {' +
-            'border-bottom: 1px solid transparent;' +
-            'border-top: 1px solid transparent;' +
-        '}' +
-        '.athing.active > td {' +
-            'border-bottom: 1px solid #888888;' +
-            'border-top: 1px solid #888888;' +
-        '}' +
-        '.athing.active > td:first-child {' +
-            'border-left: 1px solid #888888;' +
-        '}' +
-        '.athing.active > td:last-child {' +
-            'border-right: 1px solid #888888;' +
-        '}' +
-        '';
-    var head = document.getElementsByTagName( 'head' )[ '0' ];
-    head.appendChild( style );
-    function moveToPosition( position ) {
-        console.log( 'moving to position', position );
-        var activeEntry = entries.get( position );
+    },
+    _goToCurrentPosition: function() {
+        console.info( '_goToCurrentPosition', this.currentPosition );
+        var activeEntry = this.entries.get( this.currentPosition );
         if ( activeEntry ) {
-            entries.removeClass( 'active' );
-            $(activeEntry).addClass( 'active' );
-            var activeEntryOffset = $(activeEntry).offset();
+            console.log( 'focus on active entry:', activeEntry );
+            activeEntry.focus();
+            this.entries.removeClass( '_vimNavActive' );
+            $( activeEntry ).addClass( '_vimNavActive' );
+            var activeEntryOffset = $( activeEntry ).offset();
             var activeEntryTop = activeEntryOffset.top;
             var activeEntryBottom = activeEntryTop + $(activeEntry).height();
-            var scrollTop = $(window).scrollTop();
+            var scrollTop = $( window ).scrollTop();
             var newScrollTop = 0;
             if ( activeEntryBottom > scrollTop + document.body.clientHeight ) {
                 newScrollTop = activeEntryTop;
@@ -129,49 +125,76 @@ if ( /^https:\/\/news\.ycombinator\.com\/$/.test( loc ) ) {
             }
             console.log( '---' );
         }
-    }
-    var entries = $( '.athing' );
-    var currentPosition = -1;
+    },
+    _goToFirst: function() {
+        console.info( '_goToFirst' );
+        currentPosition = 0;
+        this._goToCurrentPosition();
+    },
+    _goToLast: function() {
+        console.info( '_goToLast' );
+        currentPosition = this.entries.length - 1;
+        this._goToCurrentPosition();
+    },
+    _goToNext: function() {
+        console.info( '_goToNext' );
+        if ( this.currentPosition >= this.entries.length - 1 ) {
+            this.currentPosition = 0;
+        } else {
+            this.currentPosition += 1;
+        }
+        this._goToCurrentPosition();
+    },
+    _goToPrevious: function() {
+        console.info( '_goToPrevious' );
+        if ( this.currentPosition > 0 ) {
+            this.currentPosition -= 1;
+        } else {
+            this.currentPosition = this.entries.length - 1;
+        }
+        this._goToCurrentPosition();
+    },
+    _setEntries: function( entries ) {
+        console.info( '_setEntries', entries );
+        this.entries = entries;
+        if ( ! ( this.entries.length >= 1 ) ) {
+            console.warn( 'entries found:', this.entries.length );
+        } else {
+            console.log( 'entries found:', this.entries.length );
+        }
+    },
+};
+var url = window.location.toString();
+console.log( 'url:', url );
+if ( /^https:\/\/news\.ycombinator\.com\/(news)?$/.test( url ) ) {
+    console.info( 'home page' );
+    var homePageLinks = $( '.title > a' );
+    var commentLinks = $( '.subtext a[href^=item\\?id\\=]' );
+    var links = $().add(homePageLinks).add(commentLinks);
+    $( links ).each(function() {
+        $(this).click(function( event ) {
+            console.log( event.target );
+            event.preventDefault();
+            openBackgroundTab( $(this).prop( 'href' ) );
+        });
+    });
+    var $searchInput = $( '<input placeholder="Search" type="text" />' );
+    $searchInput.keypress(function( event ) {
+        if ( event.which === keyCode.ENTER ) {
+            console.log( 'enter pressed', $(this).val() );
+            window.location = 'https://hn.algolia.com/#!/all/forever/0/' + encodeURIComponent( $(this).val() );
+        }
+    });
+    $( '.pagetop:first' ).append( ' | ' ).append( $searchInput ).parent().css({'white-space': 'nowrap'});
     $( document ).keyup(function( event ) {
         if ( event.which === keyCode.FORWARD_SLASH ) {
             $searchInput.focus();
-        } else if ( event.which === keyCode.J ||
-                    event.which === keyCode.K ) {
-            console.log( 'current position:', currentPosition );
-            if ( event.which === keyCode.J ) {
-                if ( currentPosition >= entries.length - 1 ) {
-                    currentPosition = 0;
-                } else {
-                    currentPosition += 1;
-                }
-            } else if ( event.which === keyCode.K ) {
-                if ( currentPosition > 0 ) {
-                    currentPosition -= 1;
-                } else {
-                    currentPosition = entries.length - 1;
-                }
-            }
-            moveToPosition( currentPosition );
-        } else if ( event.which === keyCode.G ) {
-            if ( event.shiftKey ) {
-                console.log( 'current position:', currentPosition );
-                currentPosition = entries.length - 1;
-                moveToPosition( currentPosition );
-            }
-        } else if ( event.which === keyCode.ENTER ) {
-            console.log( 'current position:', currentPosition );
-            if ( currentPosition >= 0 ) {
-                var activeEntry = $( $( '.athing' ).get( currentPosition ) );
-                var activeEntryLink = activeEntry.find( '.title > a' );
-                if ( event.shiftKey ) {
-                    activeEntryLink = activeEntry.next().find( '.subtext a[href^=item\\?id\\=]' ).first();
-                }
-                activeEntryLink.click();
-            }
         }
     });
-} else if ( /^https:\/\/news\.ycombinator\.com\/item\?id=/.test( loc ) ) {
+    vimNavigation.init( $( 'tr.athing > .title > a' ) );
+} else if ( /^https:\/\/news\.ycombinator\.com\/item\?id=/.test( url ) ) {
     console.info( 'item page' );
+    vimNavigation.init( $( 'html > body > center > table > tbody > tr:eq(2) > td > table:eq(1) > tbody > tr.athing' ) );
     $( 'td[class="title"]:last > a' ).click(function( event ) {
         event.preventDefault();
         openBackgroundTab( $(this).prop( 'href' ) );
